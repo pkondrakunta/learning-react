0e782a9f1898cff6db70eb7e31149f5f050e44c3
pkondrakunta <pragnyakondrakunta@gmail.com>
2022-08-15 23:46:48 -0400

    created react app and basic template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState, useEffect, useReducer } from 'react';
+
+const cities = ["Tokyo", "New York City", "Hyderabad"]
+
+function App(props) {
+
+  const [emotion, setEmotion] = useState("happy");
+  const [secondaryEmotion, setSecondary] = useState("tired")
+// Logs the emotion every time it changes
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`)
+  },[emotion])
+
+  useEffect(() => {
+    console.log(`Emotion change to: ${emotion}, ${secondaryEmotion}`)
+  },[emotion, secondaryEmotion])
+
+  const  [checked, setChecked] = useReducer((checked) => !checked);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from {props.library}
+      </h1>
+
+      <h2>Current emotion is {emotion}
+      </h2>
+      <br/>
+
+      <button onClick={() => setEmotion("sad")}>Make me sad</button>
+      <button onClick={() => setEmotion("excited")}>Make me Excited</button>
+
+      <br/>
+
+      <h2>Secondary emotion is {secondaryEmotion}
+      </h2>
+
+      <button onClick={() => setSecondary("tired")}>I am tired</button>
+      <button onClick={() => setSecondary("energetic")}>I am energetic</button>
+
+      <br/>
+
+      <h2>Using Reducer for Checked/Unchecked</h2>
+
+      <input type="checkbox"
+      value={checked}
+      onChange={setChecked} />
+
+      <label>
+        {checked ? "checked" : "unchecked"}
+      </label>
+
     </div>
   );
 }
